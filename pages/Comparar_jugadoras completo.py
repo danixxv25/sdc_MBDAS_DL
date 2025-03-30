9bd5356b00cde0b46c00400aaa40d53ec71c4e34
danixxv25 <danixxv25@gmail.com>
2025-03-30 20:32:19 +0200

    test new layout

--- pages/Comparar_jugadoras completo.py
+++ pages/Comparar_jugadoras completo.py
@@ -894,11 +894,11 @@ if df_combined is not None and not df_combined.empty:
                         <div style="font-size: 12px;">Basado en similitud de percentiles</div>
                     </div>
                     """, unsafe_allow_html=True)
-    st.markdown("<br>", unsafe_allow_html=True)
-    st.markdown("<br>", unsafe_allow_html=True)
-    st.markdown("<br>", unsafe_allow_html=True)
-    st.markdown("<br>", unsafe_allow_html=True)
-    st.markdown("<br>", unsafe_allow_html=True)
+        st.markdown("<br>", unsafe_allow_html=True)
+        st.markdown("<br>", unsafe_allow_html=True)
+        st.markdown("<br>", unsafe_allow_html=True)
+        st.markdown("<br>", unsafe_allow_html=True)
+        st.markdown("<br>", unsafe_allow_html=True)
 
         if metrics1_data and metrics2_data and player1_position == player2_position:
             # Crear gráfico radar para visualizar similitudes
