e66c4f22f6654a602b8ca3c88fc99d8d3e248695
danixxv25 <danixxv25@gmail.com>
2025-03-30 19:08:00 +0200

    test new layout

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Análisis de Jugadoras", layout="wide")
 main_page = st.Page("pages/Main_page.py", title="Inicio", icon="🏠")
 page_1= st.Page("pages/Equipo_propio copy.py", title="Equipo Propio", icon="🇵🇪")
 page_2= st.Page("pages/Buscar_jugadoras copy.py", title="Buscar Jugadoras", icon="🔎")
-page_3 = st.Page("pages/Comparar_jugadoras mejorado.py", title="Comparar Jugadoras", icon="📊")
+page_3 = st.Page("pages/Comparar_jugadoras completo.py", title="Comparar Jugadoras", icon="📊")
 page_4 = st.Page("pages/Reemplazar_jugadora.py", title="Reemplazar Jugadoras", icon="🔄")
 
 pg = st.navigation([main_page, page_1, page_2, page_3, page_4])
