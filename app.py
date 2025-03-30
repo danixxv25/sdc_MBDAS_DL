36474876e66c90eb7aedd63bfb91bc2058a48a58
danixxv25 <danixxv25@gmail.com>
2025-03-30 03:31:51 +0200

    test new layout

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from utils import display_logo
 st.set_page_config(page_title="Análisis de Jugadoras", layout="wide")
 
 main_page = st.Page("pages/Main_page.py", title="Inicio", icon="🏠")
-page_1= st.Page("pages/Equipo_propio.py", title="Equipo Propio", icon="🇵🇪")
+page_1= st.Page("pages/Equipo_propio copy.py", title="Equipo Propio", icon="🇵🇪")
 page_2= st.Page("pages/Buscar_jugadoras.py", title="Buscar Jugadoras", icon="🔎")
 page_3 = st.Page("pages/Comparar_jugadoras.py", title="Comparar Jugadoras", icon="📊")
 page_4 = st.Page("pages/Reemplazar_jugadora.py", title="Reemplazar Jugadoras", icon="🔄")
